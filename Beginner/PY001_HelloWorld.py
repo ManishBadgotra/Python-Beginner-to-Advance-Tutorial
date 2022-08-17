de096c6d83a4c9ca1abf20254134222e3217ac69
Manish Sharma <badgotra.maish@gmail.com>
2022-08-17 23:14:40 +0530

    Hello world program has been uploaded

---
+++ Beginner/PY001_HelloWorld.py
@@ -0,0 +1,10 @@
+# This is a single line comment in python and there is no multi-line comment in it. 
+# So for multiple lines you have to use hastag or pound symbole "#" in every line to use it.
+
+# lets print our hello world program.
+
+print("Hello World!") # this print() funtion prints whatever we write in either single or in double quotes.
+
+print('Hello world! written with help of single quotes') # written with the help of single quotes.
+
+
