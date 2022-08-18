abd3c8a06a7dde2241d511c0f520afd64450c00b
Manish Sharma <badgotra.maish@gmail.com>
2022-08-18 20:29:52 +0530

    Variables in python lesson added

---
+++ Beginner/PY002_Variables.py
@@ -0,0 +1,19 @@
+# Variables in python.
+# What is variables?
+# Variables are containers for storing data values.
+
+####
+##
+##      Creating Variables
+##
+####
+# Python has no command for declaring a variable like other programming languages have.
+# A variable is created the moment you first assign a value to it.
+
+x = 5
+name = "John"
+
+print(x) # prints 5 that is assigned to variable x.
+print(name) # prints John that is assigned to variable name.
+
+
